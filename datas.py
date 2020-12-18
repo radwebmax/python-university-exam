fbb1c9972fdd0327b7ac0bec156664a757e0e282
Max <68424156+radwebmax@users.noreply.github.com>
2020-12-18 12:04:18 +0200

    Add files via upload

--- datas.py
+++ datas.py
@@ -1,4 +1,23 @@
 import datetime
+import unittest
 
-print(datetime.date(2020, 12, 23).strftime("%U"))
+a = datetime.date(2020, 12, 23).strftime("%U")
+print(a)
+
+class MyTest(unittest.TestCase):
+
+    # testing queue for empty
+    def test_weeks(self):
+        self.assertNotEqual(a, 51)
+
+def sort_test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(MyTest('test_weeks'))
+    return suite
+
+
+if __name__ == '__main__':
+    import xmlrunner
+    runner = xmlrunner.XMLTestRunner(output='test-reports')
+    unittest.main(testRunner=runner)
 
